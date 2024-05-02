ac49b957e8d58b0f9a0d73a1c9a27c1e3bc6b2f2
ulfbo <ulfbourelius71@gmail.com>
2024-05-02 11:38:19 +0200

    GarageContext

--- Garage3.0/Models/Member.cs
+++ Garage3.0/Models/Member.cs
@@ -1,18 +1,14 @@
 ﻿namespace Garage3.Data
 {
-    public partial class GarageContext
+    public class Member
     {
-        public class Member
-        {
-            public int Id { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public DateTime DateOfBirth { get; set; }
-            public string PersonalIdentificationNumber { get; set; }
-            //public DateTime ProMembershipExpiration { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PersonalIdentificationNumber { get; set; }
 
-            // Relationships
-            public List<Vehicle> Vehicles { get; set; }
-        }
+        // Relationships
+        public List<Vehicle> Vehicles { get; set; }
     }
 }
