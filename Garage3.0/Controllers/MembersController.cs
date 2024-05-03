4ef76aead206de66d914170d769ced6a12d5a75b
ulfbo <ulfbourelius71@gmail.com>
2024-05-03 15:25:57 +0200

    Personal Id Check

--- Garage3.0/Controllers/MembersController.cs
+++ Garage3.0/Controllers/MembersController.cs
@@ -34,6 +34,7 @@ namespace Garage3.Controllers
             return View(sortedMembers);
         }
 
+
         // GET: Members/Details/5
         public async Task<IActionResult> Details(int? id)
         {
