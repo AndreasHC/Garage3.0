fb92ce0c798ccf83a8115c7d968b496ed16c5e40
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-05-06 17:02:00 +0200

    Recceipt functionality

---
+++ Garage3.0/ViewModels/Receipt.cs
@@ -0,0 +1,22 @@
+﻿using Garage3.Data;
+using System.Security.Permissions;
+
+namespace Garage3.ViewModels
+{
+    public class Receipt
+    {
+        public string RegistrationNumber { get; set; }
+        public DateTime ParkingTime { get; set; }
+        public DateTime CheckoutTime { get; set; }
+        public TimeSpan ParkingDuration => CheckoutTime - ParkingTime;
+        public int Price => ((int)ParkingDuration.TotalSeconds) / 1000;
+        public string ParkerName { get; set; }
+        public Receipt(Vehicle vehicle, string parkerName)
+        {
+            RegistrationNumber = vehicle.RegistrationNumber;
+            ParkingTime = vehicle.ParkingTime;
+            CheckoutTime = DateTime.Now;
+            ParkerName = parkerName;
+        }
+    }
+}
