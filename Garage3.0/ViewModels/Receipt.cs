dea0ce88e49cfd106f3423eb220f5c8dd01cb42c
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-05-07 09:18:25 +0200

    Minimum price

--- Garage3.0/ViewModels/Receipt.cs
+++ Garage3.0/ViewModels/Receipt.cs
@@ -9,7 +9,7 @@ namespace Garage3.ViewModels
         public DateTime ParkingTime { get; set; }
         public DateTime CheckoutTime { get; set; }
         public TimeSpan ParkingDuration => CheckoutTime - ParkingTime;
-        public int Price => ((int)ParkingDuration.TotalSeconds) / 1000;
+        public int Price => Math.Max(((int)ParkingDuration.TotalSeconds) / 1000, 10);
         public string ParkerName { get; set; }
         public Receipt(Vehicle vehicle, string parkerName)
         {
