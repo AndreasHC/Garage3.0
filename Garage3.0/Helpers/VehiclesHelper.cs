86292fb358b851ae44004dedbb6953c501437982
ulfbo <ulfbourelius71@gmail.com>
2024-05-08 12:35:57 +0200

    Refactored Parking cost to VehicleHelper

---
+++ Garage3.0/Helpers/VehiclesHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Garage3.Helpers
+{
+    public class VehiclesHelper
+    {
+        public static int GetParkingCost(TimeSpan parkingDuration)
+        {
+            return Math.Max(((int)parkingDuration.TotalSeconds) / 1000, 10);
+        }
+    }
+}
