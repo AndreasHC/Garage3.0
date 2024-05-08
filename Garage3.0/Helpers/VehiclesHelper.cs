3590daba2c88bdd4ff8072cda00d743a66edd5e2
ulfbo <ulfbourelius71@gmail.com>
2024-05-08 18:04:50 +0200

    commit before switching branch

--- Garage3.0/Helpers/VehiclesHelper.cs
+++ Garage3.0/Helpers/VehiclesHelper.cs
@@ -2,8 +2,14 @@
 {
     public class VehiclesHelper
     {
-        public static int GetParkingCost(TimeSpan parkingDuration)
+        public static int GetParkingCost(Data.VehicleType vehicleType, TimeSpan parkingDuration)
         {
+            double coefficient = 1.0;
+            double 
+            if (!vehicleType.SizeIsInverted)
+            {
+
+            }
             return Math.Max(((int)parkingDuration.TotalSeconds) / 1000, 10);
         }
     }
