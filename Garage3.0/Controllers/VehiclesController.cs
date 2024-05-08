88566b15a79ee02d252b25dd6e139d9e962fb92c
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-05-08 09:36:50 +0200

    Spot shown in vehicle details

--- Garage3.0/Controllers/VehiclesController.cs
+++ Garage3.0/Controllers/VehiclesController.cs
@@ -9,6 +9,7 @@ using Garage3.Data;
 using Garage3.Helpers;
 using Garage3.ViewModels;
 using Garage3.Models;
+using System.Text;
 
 namespace Garage3.Controllers
 {
@@ -84,6 +85,8 @@ namespace Garage3.Controllers
             {
                 return NotFound();
             }
+            var spots = await _context.SpotOccupations.Where(s => s.VehicleId == id).Select(s => s.SpotId).ToListAsync();
+            ViewBag.SpotList = String.Join(", ", spots);
 
             return View(vehicle);
         }
