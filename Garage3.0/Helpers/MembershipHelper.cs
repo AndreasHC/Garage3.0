d5b5d404a270f141c15f9a94e7e19194252b9e7b
ulfbo <ulfbourelius71@gmail.com>
2024-05-09 22:48:48 +0200

    Added membership helper

---
+++ Garage3.0/Helpers/MembershipHelper.cs
@@ -0,0 +1,22 @@
+﻿using Garage3.Data;
+
+namespace Garage3.Helpers
+{
+    public static class MembershipHelper
+    {
+        public static bool IsProMember(Member member)
+        {
+            return member.EndDate >= DateTime.Today;
+        }
+
+        public static double CalculateDiscount(Member member, int numberOfSpaces)
+        {
+            if (!IsProMember(member)) return 0;
+
+            var discountRate = 0.1 * numberOfSpaces;
+            discountRate = discountRate > 0.4 ? 0.4 : discountRate; // Max 40% rabatt
+            return discountRate;
+        }
+    }
+
+}
